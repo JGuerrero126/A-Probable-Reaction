247a442a6ce95962bc5649e203926eb609e453c9
Jerry Guerrero <jerry1996714@gmail.com>
2022-04-28 20:45:32 -0700

    adding files before beginning work

---
+++ src/components/pages/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        I am a coding student currently going through the coding bootcamp at
+        UCLA Extension. I enjoy collecting manga and vinyl as well as playing
+        video games. You can usually find me buried deep in some existential
+        nightmare like
+        <span>
+          <a href="https://en.wikipedia.org/wiki/Berserk_(manga)">"Berserk"</a>
+        </span>
+        or
+        <span>
+          <a href="https://en.wikipedia.org/wiki/Vagabond_(manga)">
+            "Vagabond"
+          </a>
+        </span>
+        or slaughtering demons by the hundreds in
+        <span>
+          <a href="https://en.wikipedia.org/wiki/Doom_Eternal">"DOOM"</a>
+        </span>
+        . When I'm not at my desk making all kinds of cool stuff, of course.
+        I've been hard at work to get into multiple fields including Art,
+        Writing, Programming and Designing. I hope to one day be a master of all
+        like the great
+        <span>
+          <a href="https://en.wikipedia.org/wiki/Takeshi_Kitano">
+            Beat Takeshi
+          </a>
+        </span>
+        from Japan.
+      </p>
+    </div>
+  );
+}
